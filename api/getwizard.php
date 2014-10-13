e53cf86c78aced1f5478a35fe5ce16bccebe452e
Chizbang <jamertheprogrammer@gmail.com>
2014-10-13 09:47:37 -0400

    Removed pointless file

--- api/getwizard.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once("classes/database_class.php");
-	echo "test";
-	$getProjectsQuery = new Database();
-
-	$result = $getProjectsQuery->fetchAllQuery("SELECT * FROM projects", array());
-
-	echo json_encode($result);
-?>
